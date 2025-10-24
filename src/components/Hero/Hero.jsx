e87c2564c4f020d9f21fea9faeeb0bde8a4c36d2
karimov <telegramemail09@gmail.com>
2025-10-24 17:42:28 +0500

    responsive fixed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,26 +3,28 @@ import css from "./Hero.module.css";
 export default function Hero() {
   return (
     <div>
-      <section className={`${css.hero} ${css.container}`}>
-        <div className={css["hero-content"]}>
-          <div className={css["hero-data"]}>
-            <h1 className={css["hero-title"]}>
-              More than just <br />
-              shorter links
-            </h1>
-            <p className={css["hero-text"]}>
-              Build your brand’s recognition and get detailed insights on how
-              your links are performing.
-            </p>
-            <a className={css["blue-button"]} href="#">
-              Get Started
-            </a>
+      <section className={css.hero}>
+        <div className={css.container}>
+          <div className={css["hero-content"]}>
+            <div className={css["hero-data"]}>
+              <h1 className={css["hero-title"]}>
+                More than just <br />
+                shorter links
+              </h1>
+              <p className={css["hero-text"]}>
+                Build your brand’s recognition and get detailed insights on how
+                your links are performing.
+              </p>
+              <a className={css["blue-button"]} href="#">
+                Get Started
+              </a>
+            </div>
+            <img
+              className={css["hero-img"]}
+              src="./hero-img.svg"
+              alt="human work with computer"
+            />
           </div>
-          <img
-            className={css["hero-img"]}
-            src="./hero-img.jpg"
-            alt="human work with computer"
-          />
         </div>
       </section>
     </div>
